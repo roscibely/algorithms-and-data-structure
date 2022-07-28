23c701caaa3c586e6758b3f7c7eda66ecbf1501f
roscibely <rosanacibely@gmail.com>
2022-07-28 20:22:01 -0300

    docs: added docs

--- pointers/exercise-four.c
+++ pointers/exercise-four.c
@@ -2,18 +2,24 @@
 #include <math.h>
 
 void calcula_hexagono(float l, float *area, float *perimetro){
+    /* Função que calcula a area e perimetro de uma hexagono 
 
-    *perimetro = 6*l;
-    *area = (3*pow(l,2)*sqrt(3))/2;
+    float l: lado do hexágono
+    float *area: ponteiro que armazena o endereço da memoria onde está localizado o valor para area 
+    float *perimetro: ponteiro que armazena o endereço da memoria onde está localizado o valor para perimetro
 
+    */
+    *perimetro = 6*l; // Realiza o calculo do perimetro do hexagono e atualiza o conteudo do endereço que o ponteiro perimetro aponta 
+    *area = (3*pow(l,2)*sqrt(3))/2;  // Realiza o calculo da area do hexagono e atualiza o conteudo do endereço que o ponteiro area aponta 
 }
 
 int main(void){
+    /* Função do programa principal */ 
 
-    float l, a, p;
-    scanf("%f", &l);
-    calcula_hexagono(l,&a,&p);
-    printf("Area = %.2f \n Perimetro = %.2f \n", a,p);
+    float l, a, p; // Declaração das variaveis 
+    scanf("%f", &l); // Realiza a leitura do lado de um hexágono e amarzena o valor em "l"
+    calcula_hexagono(l,&a,&p); //Faz a chamada da função calcula_hexagono
+    printf("Area = %.2f \n Perimetro = %.2f \n", a,p); // Exibe a area e o perimetro calculados pela função calcula_hexagono
 
     return 0;
 }
