5e4a9bda4a0d2516314955062269e9ff55aa696d
roscibely <rosanacibely@gmail.com>
2022-08-02 20:03:28 -0300

    docs: updated

--- pointers/exercise-five.c
+++ pointers/exercise-five.c
@@ -2,19 +2,33 @@
 
 
 char situacao(float p1, float p2, float p3, int faltas, int aulas, float*media){
-    
-    int nFaltas;
+    /* Função que  determine a média e a situação de um aluno em uma disciplina
 
-    *media = (p1+p2+p3)/3.0;
-    nFaltas = (100*faltas)/aulas;
+    float p1: nota referente a prova 1 
+    float p2: nota referente a prova 2
+    float p3: nota referente a prova 3
+    int faltas: faltas do aluno
+    int aulas: total de aulas 
+    float *media: ponteiro que armazena o endereço da variável que representa a média da nota
 
-    if ((nFaltas <=25) && (*media>=6.0)){
+    Return: 
+        A: para aprovado
+        R: para reprovado
+        F: para reprovado por faltas  
+    */
+
+    int nFaltas; // Variável para armazenar porcentagem de faltas 
+
+    *media = (p1+p2+p3)/3.0; // o conteudo do ponteiro media é modificado para a media das notas 
+    nFaltas = (100*faltas)/aulas; // porcentagem de faltas é calculada
+
+    if ((nFaltas <=25) && (*media>=6.0)){ //Verifica se o aluno foi aprovado
         return 'A';
         }
-    else if ((nFaltas <=25) && (*media<6.0)){
+    else if ((nFaltas <=25) && (*media<6.0)){ // Verifica se o aluno foi reprovado por nota 
         return 'R';
         }
-    else
+    else    // Instrução executada apenas as duas condições anteriores não forem satisfeitas. 
         return 'F';
 }
 
@@ -22,12 +36,14 @@ char situacao(float p1, float p2, float p3, int faltas, int aulas, float*media){
 
 int main (void){
 
-    float n1, n2, n3, m;
-    char s;
-    int f, a;
+    /* Função principal do programa */ 
 
-    scanf("%f %f %f %d %d", &n1 , &n2, &n3, &f, &a);
-    s = situacao(n1, n2, n3, f, a, &m);
+    float n1, n2, n3, m; //declaração de variáveis: n1, n2, n3 para as três notas a serem digitadas e m para guardar a média das notas
+    char s; // s armazenará a letra referente a situação do aluno, isto é, A, R ou F. 
+    int f, a; // f variável para armazenar o total de faltas. a variável para armazenare total de aulas 
+
+    scanf("%f %f %f %d %d", &n1 , &n2, &n3, &f, &a); 
+    s = situacao(n1, n2, n3, f, a, &m); // Faz a chamada da função situacao
 
     switch(s){
     case 'A':
