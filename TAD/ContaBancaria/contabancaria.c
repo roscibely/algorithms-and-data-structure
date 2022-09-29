83c55bef6ae8202e2cfe01e94104a85924117ec1
roscibely <rosanacibely@gmail.com>
2022-09-29 18:02:27 -0300

    refactor: refactored functions

--- TAD/ContaBancaria/contabancaria.c
+++ TAD/ContaBancaria/contabancaria.c
@@ -12,37 +12,42 @@ struct contabancaria
 
 
 ContaBancaria *criaconta(char * nome_arquivo){
+
     ContaBancaria *conta;
     conta = (ContaBancaria *) malloc(sizeof(ContaBancaria));
+    if (conta == NULL){
+        printf("Erro ao alocar memoria");
+        exit(1);
+    }
+
     printf("Digite o nome do titular: ");
     scanf("%s", conta->titular);
     printf("Digite o numero da conta: ");
     scanf("%d", &conta->numero);
     conta->saldo = 0;
+
     FILE *arq;
     arq = fopen(nome_arquivo, "a");
+    if(arq == NULL){
+        printf("Erro ao abrir o arquivo");
+        exit(1);
+    }
     fprintf(arq, "Titular: %s Numero: %d Saldo: %.2f \n", conta->titular, conta->numero, conta->saldo);
     fclose(arq);
+
     return conta;
 }
 
 
-void deposita(ContaBancaria *conta, float valor, char * nome_arquivo){
+void deposita(ContaBancaria *conta, float valor){
     conta->saldo += valor;
-    FILE *arq;
-    arq = fopen(nome_arquivo, "a");
-    fprintf(arq, "Titular: %s Numero: %d Saldo: %.2f \n", conta->titular, conta->numero, conta->saldo);
-    fclose(arq);
 }
 
 
-void saca(ContaBancaria *conta, float valor, char * nome_arquivo){
+void saca(ContaBancaria *conta, float valor){
+
     if(conta->saldo >= valor){
         conta->saldo -= valor;
-        FILE *arq;
-        arq = fopen(nome_arquivo, "a");
-        fprintf(arq, "Titular: %s Numero: %d Saldo: %.2f \n", conta->titular, conta->numero, conta->saldo);
-        fclose(arq);
     }else{
         printf("Saldo insuficiente para realizar o saque");
     }
@@ -51,6 +56,7 @@ void saca(ContaBancaria *conta, float valor, char * nome_arquivo){
 
 
 void transfere(ContaBancaria *conta1, ContaBancaria *conta2, float valor){
+    
     if(conta1->saldo >= valor){
         conta1->saldo -= valor;
         conta2->saldo += valor;
