b6fb0965de0e445f521c7f786363b02aa913b0fb
roscibely <rosanacibely@gmail.com>
2023-02-08 15:46:25 -0300

    feat: added aninhamento

---
+++ estruturas/aninhamento.c
@@ -0,0 +1,61 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+typedef struct disciplina {
+    char nome[50];
+    int codigo;
+    int carga_horaria;
+} Disciplina;
+
+typedef struct aluno {
+    char nome[50];
+    int matricula;
+    int cpf;
+    Disciplina *disciplina; 
+} Aluno;
+
+
+Disciplina * aloca_disciplina(int num_disciplinas) {
+    Disciplina *disciplina = (Disciplina *) malloc(num_disciplinas*sizeof(Disciplina));
+    if (disciplina == NULL){
+        exit(1);
+    }   
+    return disciplina;
+}
+
+Aluno * aloca_aluno() {
+    Aluno *aluno = (Aluno *) malloc(sizeof(Aluno));
+    if (aluno == NULL)
+    {
+        exit(1);
+    }   
+    aluno->disciplina = aloca_disciplina(1);
+    return aluno;
+}
+
+void inicializar_aluno(Aluno *aluno) {
+    printf("Digite o nome do aluno: ");
+    scanf("%s", aluno->nome);
+    printf("Digite a matricula do aluno: ");
+    scanf("%d", &aluno->matricula);
+    printf("Digite o cpf do aluno: ");
+    scanf("%d", &aluno->cpf);
+    printf("Digite o nome da disciplina: ");
+    scanf("%s", aluno->disciplina->nome);
+    printf("Digite o codigo da disciplina: ");
+    scanf("%d", &aluno->disciplina->codigo);
+    printf("Digite a carga horaria da disciplina: ");
+    scanf("%d", &aluno->disciplina->carga_horaria);
+}
+
+void imprime_aluno(Aluno *aluno){
+    printf(" Nome do aluno: %s, Matricula: %d, CPF: %d ", aluno->nome, aluno->matricula, aluno->cpf);
+    printf(" Nome da disciplina: %s, Codigo: %d, Carga Horaria: %d ", aluno->disciplina->nome, aluno->disciplina->codigo, aluno->disciplina->carga_horaria);
+}
+
+int main(void){
+    Aluno *aluno = aloca_aluno();
+    inicializar_aluno(aluno);
+    imprime_aluno(aluno);
+    return 0;
+}
